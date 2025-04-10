8ee4ba1a9a8bb0ff5215d7d27301b4f933e810bc
Rodner jose <108538585+rodnerjose@users.noreply.github.com>
2025-04-10 10:45:59 -0300

    çjhç

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-onst botoes = document.querySelectorAll(".botao");
+ botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
 botoes[i].onclick = function(){
 
 for(let j=0;j<botoes.length;j++){
-botoes[j].classList.remove("ativo");
+botoes[j].classLisonstt.remove("ativo");
 }
 botoes[i].classList.add("ativo");
 }
